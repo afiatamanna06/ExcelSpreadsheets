8a31dcfc874a8b2d1bfd4a371d8f94e64ebb8c2e
afiatamanna06 <afiatamanna@gmail.com>
2020-05-15 23:09:56 +0600

    app.py updated

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ cell = sheet.cell(1, 1)
 
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    print(cell.value)
+    corrected_price = cell.value*0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transactions2.xlsx')
