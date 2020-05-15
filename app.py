30c72ffc7843575099da4685bcc9362a967707b2
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-15 23:27:01 +0600

    app.py updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ cell = sheet.cell(1, 1)
 
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    corrected_price = cell.value*0.9
+    corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
 
